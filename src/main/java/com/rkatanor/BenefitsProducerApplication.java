95cfa45efadc0a0065e44f2323449b232f699d80
Rajkumar Katanor <rkatanor@valuelabs.com>
2019-05-17 19:53:54 +0530

    added springbootservletinitilizer file

--- src/main/java/com/rkatanor/BenefitsProducerApplication.java
+++ src/main/java/com/rkatanor/BenefitsProducerApplication.java
@@ -2,12 +2,19 @@ package com.rkatanor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BenefitsProducerApplication {
+public class BenefitsProducerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BenefitsProducerApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(BenefitsProducerApplication.class);
 	}
 
 }
